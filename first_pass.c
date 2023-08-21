82ee95838f3ce7a2596c0c61331d83657341b477
carmielmaximius <140946045+carmielmaximius@users.noreply.github.com>
2023-08-21 10:57:36 +0300

    Update first_pass.c

--- first_pass.c
+++ first_pass.c
@@ -1,4 +1,4 @@
-void make_line_binary(char st[], int line){
+int make_line_binary(char st[], int line){
   int i = 0, line = 0;
   int res = add_string_data(st, line);
   if(res == 1){
@@ -13,6 +13,8 @@ void make_line_binary(char st[], int line){
       if(res == 2)
         res = make_extern_data(st, line);
     }
+    if(res == 0)
+      return 0;
+    return 1;
   }
-
 }
