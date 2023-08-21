cd333436ebf83f768a7ea83b7e1102617325e6c9
inbar360 <inbarot21@gmail.com>
2023-08-21 14:09:37 +0300

    Update sec_pass.c

---
+++ sec_pass.c
@@ -0,0 +1,21 @@
+#include "second_pass.h"
+
+boolean make_code_binary(FILE *from, struct Data_Table *data_head, int *IC, int *DC, int *counter) {
+    struct Data_Table *curr_node;
+    int i = 0, line;
+    char st[MAX_LINE];
+    for (line = 1; fgets(st, MAX_LINE, from) != NULL; line++) {
+        curr_node = data_head;
+        mark_label_entry(st, line, data_head, counter);
+        while(curr_node && (curr_node) != 'c' && getType(curr_node) != 'E'){
+            curr_node = getNext(curr_node);
+        }
+        int res = make_command(st, curr_node, line, data_head, counter);
+        if(res == 1) {
+            setType(curr_node, 'f'); //stands for finished with this line
+        }
+        if(res == FALSE)
+          return FALSE;
+    }
+}
+
