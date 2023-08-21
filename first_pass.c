708451817d9c647e3c23079f3a33d0b927a7df11
inbar360 <inbarot21@gmail.com>
2023-08-21 11:26:53 +0300

    add

--- first_pass.c
+++ first_pass.c
@@ -1,20 +1,22 @@
-int make_line_binary(char st[], int line){
-  int i = 0, line = 0;
-  int res = add_string_data(st, line);
-  if(res == 1){
-    res = add_extern_data(st, line);
-    if(res == 2)
-      res = make_memory_of_command(st, line);
-  }
-  else if(res == 2){
-    res = add_data_data(st, line);
-    if(res == 2){
-      res = make_memory_of_command_label(st, line);
+#include "first_pass.h"
+
+boolean firstpass_line(char st[], int line){
+    
+    int res = add_string_data(st, line);
+    if(res == 1){
+      res = add_extern_data(st, line);
       if(res == 2)
-        res = make_extern_data(st, line);
+        res = make_memory_of_command(st, line);
     }
-    if(res == 0)
-      return 0;
-    return 1;
-  }
+    else if(res == 2){
+      res = add_data_data(st, line);
+      if(res == 2){
+        res = make_memory_of_command_label(st, line);
+        if(res == 2)
+          res = make_extern_data(st, line);
+      }
+      if(res == 0)
+        return FALSE;
+      return TRUE;
+    }
 }
---
+++ first_pass.h
@@ -0,0 +1,4 @@
+#include "globals.h"
+#include "pass_functions.h"
+
+boolean firstpass_line(char st[], int line);
